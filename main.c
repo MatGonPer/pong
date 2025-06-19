a0ef4517d7b9bdb16a1772a90d8cd3c4cb09fb7b
Matheus Gonçalves <matheushtht@gmail.com>
2025-06-19 16:31:37 -0300

    Criando jogador, oponente e bola, e desenhando-os na tela

--- main.c
+++ main.c
@@ -10,10 +10,32 @@ int main() {
     InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Pong");
     SetTargetFPS(60);
 
+    //Jogador
+    float playerX = 50;
+    float playerY = (SCREEN_HEIGHT / 2) - 50;
+    float playerWidth = 10;
+    float playerHeight = 100;
+
+    //Oponent
+    float opponentX = SCREEN_WIDTH - 50 - 10;
+    float opponentY = (SCREEN_HEIGHT / 2) - 50;
+    float opponentWidth = 10;
+    float opponentHeight = 100;
+
+    //Bola
+    float ballX = SCREEN_WIDTH / 2;
+    float ballY = SCREEN_HEIGHT / 2;
+    float ballRadius = 8;
+
     while(!WindowShouldClose()) {
         BeginDrawing();
-        ClearBackground(BLACK);
-        DrawText("Game Loop funcionando!", 200, 150, 30, WHITE);
+            ClearBackground(BLACK);
+            //Desenha jogador
+            DrawRectangle(playerX, playerY, playerWidth, playerHeight, WHITE);
+            //Desenha oponente
+            DrawRectangle(opponentX, opponentY, opponentWidth, opponentHeight, WHITE);
+            //Desenha bola
+            DrawCircle(ballX, ballY, ballRadius, WHITE);
         EndDrawing();
     }
 
